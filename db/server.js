55d9d03e2bda2c5ad579d752b8866e5c66c0e775
Jess <jess.m.jerez@gmail.com>
2023-05-03 16:21:24 -0600

    created gitignore,packagelock json

--- db/server.js
+++ db/server.js
@@ -7,3 +7,12 @@ app.use(express.json());
 app.use(express.urlencoded(( extended: true )));
 app.use(express.static("./public"));
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT,() => console.log("server running onPORT", PORT));
