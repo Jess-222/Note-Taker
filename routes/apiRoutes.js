23c699844800e1f2c6df71368aef772d040b8e6f
Jess <jess.m.jerez@gmail.com>
2023-05-04 14:31:14 -0600

    finish api routes to include delete and edit get

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,40 @@
 const fs = require("fs");
 const path = require("path");
-const notes = require("../db/db.json");
+const notesData = require("../db/db.json");
 const express = require("express");
 const router = express.Router();
 
+
+let notes = notesData;
+
+const saveDB = () => {
+    fs.writeFileSync(
+        path.resolve(__dirname, "../db/db.json"),
+        JSON.stringify(notes)
+    );
+};
+
 router.get("/notes", (req, res) => {
+    notes = notes.map((note, index) => ({
+        ...note,
+        id: index +1,
+    }));
+
+    console.log(notes);
+    res.json(notes);
+});
+
+
+router.post("/notes", (req, res) => {
+    console.log(req.body);
+    notes.push(req.body);
+    saveDB(notes);
+    res.json(notes);
+});
+
+router.delete("/notes/:id", (req, res) => {
+    console.log(req.params.id);
+    notes.splice(req.params.id, 1);
     res.json(notes);
 });
 
