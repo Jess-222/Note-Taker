e8a1ac86ceba172aa4f6b74ad55ab5f5caafc026
Jess <jess.m.jerez@gmail.com>
2023-05-04 13:46:29 -0600

    set up apiRoutes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
 const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded(( extended: true )));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
+app.use("/api", apiRoutes);
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
@@ -15,4 +18,4 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.listen(PORT,() => console.log("server running onPORT", PORT));
+app.listen(PORT,() => console.log("server running on PORT", PORT));
